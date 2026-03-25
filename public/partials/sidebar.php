7ee987b03b0cd93fea91f8cdd1237e794af38a6f
TaniyanR <toolhouse@24.am>
2026-03-25 18:50:21 +0900

    Fix duplicate items in list and adjust sidebar links

--- public/partials/sidebar.php
+++ public/partials/sidebar.php
@@ -46,11 +46,6 @@ try {
         </form>
     </section>
 
-    <section class="sidebar-block sidebar-block--ad only-pc">
-        <h2 class="sidebar-block__title">広告</h2>
-        <div class="site-ad site-ad--rectangle"><?php render_ad('header_left_728x90', $pageType, 'pc'); ?></div>
-    </section>
-
     <section class="sidebar-block">
         <h2 class="sidebar-block__title">固定ページ</h2>
         <?php if ($fixedPages === []): ?>
@@ -58,7 +53,7 @@ try {
         <?php else: ?>
             <ul class="sidebar-links sidebar-links--pages">
                 <?php foreach ($fixedPages as $page): ?>
-                    <li><a href="<?= e(public_url('page.php?slug=' . (string)$page['slug'])) ?>"><?= e((string)$page['title']) ?></a></li>
+                    <li>・ <a href="<?= e(public_url('page.php?slug=' . (string)$page['slug'])) ?>"><?= e((string)$page['title']) ?></a></li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
@@ -91,7 +86,7 @@ try {
         <?php else : ?>
             <ul class="sidebar-links sidebar-links--partners sidebar-links--scroll">
                 <?php foreach ($partnerLinks as $link) : ?>
-                    <li><a href="<?= e((string)$link['url']) ?>" target="_blank" rel="noopener noreferrer"><?= e((string)$link['name']) ?></a></li>
+                    <li>・ <a href="<?= e((string)$link['url']) ?>" target="_blank" rel="noopener noreferrer"><?= e((string)$link['name']) ?></a></li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
