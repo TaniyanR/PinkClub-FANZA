e6a47093867c9dc9e2df33ffb9c319bf4a2bc88a
TaniyanR <toolhouse@24.am>
2026-06-17 22:17:26 +0900

    Fix admin index bootstrap fallback

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,14 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../public/_bootstrap.php';
+$publicBootstrapPath = __DIR__ . '/../public/_bootstrap.php';
+if (is_file($publicBootstrapPath)) {
+    require_once $publicBootstrapPath;
+} else {
+    require_once __DIR__ . '/../lib/bootstrap.php';
+    require_once __DIR__ . '/../lib/access_analytics.php';
+    analytics_track_request();
+}
 
 $requestPath = (string) parse_url((string) ($_SERVER['REQUEST_URI'] ?? ''), PHP_URL_PATH);
 
