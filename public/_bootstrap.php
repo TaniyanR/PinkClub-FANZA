3177322333dbaa5d26dcf025b8922f523576b314
TaniyanR <toolhouse@24.am>
2026-07-18 08:08:32 +0900

    閲覧専用ページのHTMLキャッシュを10分へ拡大

--- public/_bootstrap.php
+++ public/_bootstrap.php
@@ -10,5 +10,28 @@ require_once __DIR__ . '/../lib/public_page_cache.php';
 pcf_crawler_guard_check();
 
 $publicScriptName = basename((string)($_SERVER['SCRIPT_NAME'] ?? ''));
-$publicPageCacheTtl = in_array($publicScriptName, ['actresses.php', 'actress.php'], true) ? 600 : 120;
+$longCachePublicPages = [
+    'index.php',
+    'items.php',
+    'item.php',
+    'search.php',
+    'actresses.php',
+    'actress.php',
+    'genres.php',
+    'genre.php',
+    'makers.php',
+    'maker.php',
+    'series.php',
+    'series_list.php',
+    'series_detail.php',
+    'series_one.php',
+    'labels.php',
+    'label.php',
+    'authors.php',
+    'author.php',
+    'posts.php',
+    'post.php',
+    'page.php',
+];
+$publicPageCacheTtl = in_array($publicScriptName, $longCachePublicPages, true) ? 600 : 120;
 pcf_public_page_cache_start($publicPageCacheTtl);
